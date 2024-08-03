d39bee95b9ed7dd52eb09188767893081e42e429
Ali Mobarekati <ali7919mb@yahoo.com>
2024-08-03 10:22:14 +0330

    Init

---
+++ Assets/Scripts/LLM/ChatBot.cs
@@ -0,0 +1,181 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using LLMUnity;
+
+namespace LLMUnitySamples
+{
+    public class ChatBot : MonoBehaviour
+    {
+        public Transform chatContainer;
+        public Color playerColor = new Color32(81, 164, 81, 255);
+        public Color aiColor = new Color32(29, 29, 73, 255);
+        public Color fontColor = Color.white;
+        public Font font;
+        public int fontSize = 16;
+        public int bubbleWidth = 600;
+        public LLMCharacter llmCharacter;
+        public float textPadding = 10f;
+        public float bubbleSpacing = 10f;
+        public Sprite sprite;
+
+        [SerializeField] private GameObject inputBlocker;
+
+        private InputBubble inputBubble;
+        private List<Bubble> chatBubbles = new List<Bubble>();
+        private bool blockInput = true;
+        private BubbleUI playerUI, aiUI;
+        private bool warmUpDone = false;
+        private int lastBubbleOutsideFOV = -1;
+
+        void Start()
+        {
+            if (font == null) font = Resources.GetBuiltinResource<Font>("LegacyRuntime.ttf");
+            playerUI = new BubbleUI
+            {
+                sprite = sprite,
+                font = font,
+                fontSize = fontSize,
+                fontColor = fontColor,
+                bubbleColor = playerColor,
+                bottomPosition = 0,
+                leftPosition = 0,
+                textPadding = textPadding,
+                bubbleOffset = bubbleSpacing,
+                bubbleWidth = bubbleWidth,
+                bubbleHeight = -1
+            };
+            aiUI = playerUI;
+            aiUI.bubbleColor = aiColor;
+            aiUI.leftPosition = 1;
+
+            inputBubble = new InputBubble(chatContainer, playerUI, "InputBubble", "Loading...", 4);
+            inputBubble.AddSubmitListener(onInputFieldSubmit);
+            inputBubble.AddValueChangedListener(onValueChanged);
+            inputBubble.setInteractable(false);
+            _ = llmCharacter.Warmup(WarmUpCallback);
+        }
+
+        public void SubmitVoiceCommand(string message) {
+            inputBubble.SetText(message);
+            onInputFieldSubmit(message);
+        }
+
+        void onInputFieldSubmit(string newText)
+        {
+            inputBubble.ActivateInputField();
+            if (blockInput || newText.Trim() == "" || Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+            {
+                StartCoroutine(BlockInteraction());
+                return;
+            }
+            blockInput = true;
+            // replace vertical_tab
+            string message = inputBubble.GetText().Replace("\v", "\n");
+
+            
+            Bubble playerBubble = new Bubble(chatContainer, playerUI, "PlayerBubble", message);
+            Bubble aiBubble = new Bubble(chatContainer, aiUI, "AIBubble", "...");
+            chatBubbles.Add(playerBubble);
+            chatBubbles.Add(aiBubble);
+            playerBubble.OnResize(UpdateBubblePositions);
+            aiBubble.OnResize(UpdateBubblePositions);
+
+            Task chatTask = llmCharacter.Chat(message, aiBubble.SetText, AllowInput);
+            inputBubble.SetText("");
+        }
+
+        public void WarmUpCallback() {
+            inputBlocker.SetActive(false);
+            warmUpDone = true;
+            inputBubble.SetPlaceHolderText("Message me");
+            AllowInput();
+        }
+
+        public void AllowInput()
+        {
+            blockInput = false;
+            inputBubble.ReActivateInputField();
+        }
+
+        public void CancelRequests()
+        {
+            llmCharacter.CancelRequests();
+            AllowInput();
+        }
+
+        IEnumerator<string> BlockInteraction()
+        {
+            // prevent from change until next frame
+            inputBubble.setInteractable(false);
+            yield return null;
+            inputBubble.setInteractable(true);
+            // change the caret position to the end of the text
+            inputBubble.MoveTextEnd();
+        }
+
+        void onValueChanged(string newText)
+        {
+            // Get rid of newline character added when we press enter
+            if (Input.GetKey(KeyCode.Return))
+            {
+                if (inputBubble.GetText().Trim() == "")
+                    inputBubble.SetText("");
+            }
+        }
+
+        public void UpdateBubblePositions()
+        {
+            float y = inputBubble.GetSize().y + inputBubble.GetRectTransform().offsetMin.y + bubbleSpacing;
+            float containerHeight = chatContainer.GetComponent<RectTransform>().rect.height;
+            for (int i = chatBubbles.Count - 1; i >= 0; i--)
+            {
+                Bubble bubble = chatBubbles[i];
+                RectTransform childRect = bubble.GetRectTransform();
+                childRect.anchoredPosition = new Vector2(childRect.anchoredPosition.x, y);
+
+                // last bubble outside the container
+                if (y > containerHeight && lastBubbleOutsideFOV == -1)
+                {
+                    lastBubbleOutsideFOV = i;
+                }
+                y += bubble.GetSize().y + bubbleSpacing;
+            }
+        }
+
+        void Update()
+        {
+            if (!inputBubble.inputFocused() && warmUpDone)
+            {
+                inputBubble.ActivateInputField();
+                StartCoroutine(BlockInteraction());
+            }
+            if (lastBubbleOutsideFOV != -1)
+            {
+                // destroy bubbles outside the container
+                for (int i = 0; i <= lastBubbleOutsideFOV; i++)
+                {
+                    chatBubbles[i].Destroy();
+                }
+                chatBubbles.RemoveRange(0, lastBubbleOutsideFOV + 1);
+                lastBubbleOutsideFOV = -1;
+            }
+        }
+
+        public void ExitGame()
+        {
+            Debug.Log("Exit button clicked");
+            Application.Quit();
+        }
+
+        bool onValidateWarning = true;
+        void OnValidate()
+        {
+            if (onValidateWarning && !llmCharacter.remote && llmCharacter.llm != null && llmCharacter.llm.model == "")
+            {
+                Debug.LogWarning($"Please select a model in the {llmCharacter.llm.gameObject.name} GameObject!");
+                onValidateWarning = false;
+            }
+        }
+    }
+}
